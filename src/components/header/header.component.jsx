e9d0deeb0d652cb11ebd79bbd9e097e9062c9c45
sethrandalldev <sethjrandall@gmail.com>
2020-08-21 09:38:30 -0500

    [ADDED] styled components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
@@ -11,33 +10,36 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 
-import './header.styles.scss';
+import { 
+  OptionDiv, 
+  OptionLink, 
+  OptionsContainer, 
+  HeaderContainer, 
+  LogoContainer
+} from './header.styles';
 
 const Header = ({ currentUser, hidden }) => (
-  <div className='header'>
-    <Link className='logo-container' to="/">
+  <HeaderContainer>
+    <LogoContainer to="/">
       <Logo className='logo' />
-    </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
+    </LogoContainer>
+    <OptionsContainer>
+      <OptionLink className='option' to='/shop'>
         SHOP
-      </Link>
-      {/* <Link className='option' to='/about'>
-        ABOUT
-      </Link> */}
-      <Link className='option' to='/shop'>
+      </OptionLink>
+      <OptionLink className='option' to='/shop'>
         CONTACT
-      </Link>
+      </OptionLink>
       {
         currentUser ?
-        <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+        <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
         :
-        <Link className='option' to='/signin'>SIGN IN</Link>  
+        <OptionLink to='/signin'>SIGN IN</OptionLink>  
       }
       <CartIcon />
-    </div>
+    </OptionsContainer>
     {hidden ? null : <CartDropdown />}
-  </div>
+  </HeaderContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
