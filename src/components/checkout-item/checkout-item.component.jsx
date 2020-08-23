9038b80e2505ba215e940e14b8dc5f06295e1dd6
sethrandalldev <sethjrandall@gmail.com>
2020-08-23 12:44:45 -0500

    [CHANGED] collection and checkout components to styled-components

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -3,24 +3,34 @@ import { connect} from 'react-redux';
 
 import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions';
 
-import './checkout-item.styles.scss';
+import { 
+  CheckoutItemContainer, 
+  RemoveButtonContainer, 
+  ImageContainer,
+  Image,
+  NameContainer,
+  ValueContainer,
+  PriceContainer,
+  QuantityContainer,
+  ArrowContainer
+} from './checkout-item.styles';
 
 const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
   const { name, imageUrl, price, quantity} = cartItem
   return (
-    <div className='checkout-item'>
-      <div className='image-container'>
-        <img alt='item' src={imageUrl}/>
-      </div>
-      <span className='name'>{name}</span>
-      <span className='quantity'>
-        <div className='arrow' onClick={() => removeItem(cartItem)}>&#10094;</div>
-        <span className='value'>{quantity}</span>
-        <div className='arrow' onClick={() => addItem(cartItem)}>&#10095;</div>
-      </span>
-      <span className='price'>{price}</span>
-      <div className='remove-button' onClick={() => clearItem(cartItem)}>&#10005;</div>
-    </div>
+    <CheckoutItemContainer>
+      <ImageContainer>
+        <Image alt='item' src={imageUrl}/>
+      </ImageContainer>
+      <NameContainer>{name}</NameContainer>
+      <QuantityContainer>
+        <ArrowContainer onClick={() => removeItem(cartItem)}>&#10094;</ArrowContainer>
+        <ValueContainer>{quantity}</ValueContainer>
+        <ArrowContainer onClick={() => addItem(cartItem)}>&#10095;</ArrowContainer>
+      </QuantityContainer>
+      <PriceContainer>{price}</PriceContainer>
+      <RemoveButtonContainer onClick={() => clearItem(cartItem)}>&#10005;</RemoveButtonContainer>
+    </CheckoutItemContainer>
   );
 };
 
---
+++ src/components/checkout-item/checkout-item.styles.jsx
@@ -0,0 +1,51 @@
+import styled, { css } from 'styled-components';
+
+const checkoutItemDetailsStyles = css`
+  width: 23%;
+`;
+
+export const CheckoutItemContainer = styled.div`
+  width: 100%;
+  display: flex;
+  min-height: 100px;
+  border-bottom: 1px solid darkgrey;
+  padding: 15px 0;
+  font-size: 20px;
+  align-items: center;
+`;
+
+export const RemoveButtonContainer = styled.div`
+  padding-left: 12px;
+  cursor: pointer;
+`;
+
+export const ImageContainer = styled.div`
+  width: 23%;
+  padding-right: 15px;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+export const NameContainer = styled.span`
+  ${checkoutItemDetailsStyles}
+`;
+
+export const ValueContainer = styled.span`
+  margin: 0 10px;
+`;
+
+export const PriceContainer = styled.span`
+  ${checkoutItemDetailsStyles}
+`;
+
+export const QuantityContainer = styled.span`
+  ${checkoutItemDetailsStyles},
+  display: flex
+`;
+
+export const ArrowContainer = styled.div`
+  cursor: pointer;
+`;
